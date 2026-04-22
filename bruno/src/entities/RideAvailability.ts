4e82a524e910f9cb7a9c0f4f2b0b5e4461ecd385
Dipesh <dipesh.neupane@smail.astate.edu>
2026-04-21 22:58:25 -0500

    fix Bruno collection as regular folder

---
+++ bruno/src/entities/RideAvailability.ts
@@ -0,0 +1,51 @@
+import {
+  BeforeInsert,
+  Check,
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryColumn,
+  Relation,
+} from 'typeorm';
+import { v7 as uuidv7 } from 'uuid';
+import { User } from './User.js';
+
+@Entity()
+@Check(`"availableSeats" >= 1`)
+export class RideAvailability {
+  @PrimaryColumn()
+  rideAvailabilityId: string;
+
+  @BeforeInsert()
+  generateId(): void {
+    this.rideAvailabilityId = uuidv7();
+  }
+
+  @Column()
+  driverId: string;
+
+  @ManyToOne(() => User, (user) => user.userId)
+  driver: Relation<User>;
+
+  @Column()
+  pickupLocation: string;
+
+  @Column()
+  destination: string;
+
+  @Column({ type: 'timestamptz' })
+  departureTime: Date;
+
+  @Column({ default: 1 })
+  availableSeats: number;
+
+  @Column({ type: 'text', nullable: true })
+  notes: string | null;
+
+  @Column({ default: 'open' })
+  status: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
