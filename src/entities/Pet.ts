ed74b0ada3dc0baf15a0432cd461c4e3a1bfcddc
Sashank <dhakalsashank26@.com>
2026-04-22 11:33:35 -0500

    https://github.com/ndipesh/CampusRide

--- src/entities/Pet.ts
+++
@@ -1,9 +0,0 @@
-export type Pet = {
-  id: number;
-  name: string;
-  species: 'cat' | 'dragon' | 'blob' | 'plant' | 'rock';
-  happiness: number;
-  hunger: number;
-  energy: number;
-  lastFedAt: Date;
-};
